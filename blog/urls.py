850ce2c520fcbdfdff02a61c68a85fccec5e48f5
nathias <n@th1.as>
2018-07-17 19:40:06 +0200

    re_path instead of url for regex in django 2.0

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url, include
+from django.urls import re_path
 from django.views.generic import ListView, DetailView
 from blog.models import Post
 
 urlpatterns = [
-   url(r'^$',
+   re_path(r'^$',
        ListView.as_view(
          queryset=Post.objects.all().order_by("-date")[:25],
          template_name="blog/blog.html")),
