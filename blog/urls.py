eca15d61d1903d8bcf861e1def5095ff4b22da40
nathias <n@th1.as>
2018-07-17 19:23:49 +0200

    blog template

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from django.views.generic import ListView, DetailView
 from blog.models import Post
 
 urlpatterns = [
-   url(r'^$', ListView.as_view(
-        queryset=Post.objects.all().order_by("-date")[:25],
-        template_name="blog/blog.html")),
-            ]
+   url(r'^$',
+       ListView.as_view(
+         queryset=Post.objects.all().order_by("-date")[:25],
+         template_name="blog/blog.html")),
+              ]
